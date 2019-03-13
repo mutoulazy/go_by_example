da50ea7dbbbb28d6da80b16cb2161f81553ae828
mutoulazy <mutoulazy@gmail.com>
2019-03-13 17:25:07 +0800

    新增通道非柱塞，关闭，遍历的测试方法

--- channel/channelStd.go
+++ channel/channelStd.go
@@ -112,13 +112,84 @@ func testChannelTimeout() {
 		fmt.Println("timeout 3")
 	}
 }
+
+func testNoBlockingChannel() {
+	messages := make(chan string)
+	signals := make(chan bool)
+
+	select {
+	case msg := <-messages:
+		fmt.Println("received message ", msg)
+	default:
+		fmt.Println("no message received")
+	}
+
+	msg := "hi"
+    select {
+    case messages <- msg:
+        fmt.Println("sent message", msg)
+    default:
+        fmt.Println("no message sent")
+    }
+
+	select {
+	case msg := <-messages:
+		fmt.Println("received message ", msg)
+	case sig := <-signals:
+		fmt.Println("received signal", sig)
+	default:
+		fmt.Println("no message received")
+	}
+}
+
+func testChannelClosed() {
+	jobs := make(chan int, 5)
+	done := make(chan bool)
+	
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+                fmt.Println("received job", j)
+            } else {
+                fmt.Println("received all jobs")
+                done <- true
+                return
+            }
+		}
+	}()
+
+	for j := 1; j <= 3; j++ {
+        jobs <- j
+        fmt.Println("sent job", j)
+    }
+ 
+	close(jobs)
+	fmt.Println("sent all jobs")
+
+	<-done
+}
+
+func testRangeChannel() {
+	queue := make(chan string, 2)
+	queue <- "one";
+	queue <- "two";
+	close(queue)
+
+	for elem := range queue {
+		fmt.Println(elem)
+	}
+}
  
 func main() {
 	
-	testChannel()
-	testChannelBuf()
-	testChannelSync()
-	testChannelDirction()
-	testChannelSelector()
-	testChannelTimeout()
+	// testChannel()
+	// testChannelBuf()
+	// testChannelSync()
+	// testChannelDirction()
+	// testChannelSelector()
+	// testChannelTimeout()
+	// testNoBlockingChannel()
+	// testChannelClosed()
+	testRangeChannel()
 }
