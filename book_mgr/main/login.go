19161331b3e0614974517947597c49498e78b5bd
mutoulazy <mutoulazy@gmail.com>
2019-06-06 16:56:28 +0800

    终端图书管理系统

---
+++ book_mgr/main/login.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"../model"
+	"strings"
+)
+
+func inputUser() (username, passwd string) {
+	fmt.Printf("username:")
+	fmt.Scan(&username)
+	fmt.Printf("passwd:")
+	fmt.Scan(&passwd)
+
+	username = strings.TrimSpace(username)
+	passwd = strings.TrimSpace(passwd)
+	return
+}
+
+func adminLogin() (user *model.User, err error) {
+
+	username, passwd := inputUser()
+	user, err = mgr.AdminLogin(username, passwd)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func userLogin() (user *model.User, err error) {
+
+	username, passwd := inputUser()
+	user, err = mgr.UserLogin(username, passwd)
+	if err != nil {
+		return
+	}
+
+	return
+}
