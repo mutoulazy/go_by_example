34989c5590197b6e269316de918e3a26487780a8
mutoulazy <mutoulazy@gmail.com>
2019-07-01 15:30:17 +0800

    更新go 单元测试的例子

--- unittest/calc_test.go
+++ unittest/calc_test.go
@@ -1,10 +1,11 @@
-package main 
+package main
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestAdd(t *testing.T) {
+func testAdd(t *testing.T) {
 	r := add(2, 4)
 	if r != 6 {
 		t.Fatalf("add r error %d", r)
@@ -12,10 +13,31 @@ func TestAdd(t *testing.T) {
 	t.Logf("add success")
 }
 
-func TestSub(t *testing.T) {
+func testSub(t *testing.T) {
 	r := sub(4, 2)
 	if r != 2 {
 		t.Fatalf("sub r error %d", r)
 	}
 	t.Logf("sub success")
+}
+
+func TestAll(t *testing.T) {
+	t.Run("TestAdd", testAdd)
+	t.Run("TestSub", testSub)
+}
+
+func TestMain(m *testing.M) {
+	fmt.Println("test begin... 可以做一些初始化操作")
+	m.Run()
+}
+
+func testA(n int) int {
+	return n
+}
+
+// go的性能测试 go test -bench='.'
+func BenchmarkAll(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		testA(n)
+	}
 }
