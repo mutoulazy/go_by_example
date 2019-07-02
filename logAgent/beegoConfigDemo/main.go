3509dc7f45755dae8671d212afb5158fab90931b
mutoulazy <mutoulazy@gmail.com>
2019-07-02 17:21:59 +0800

    更新logAgent项目示例代码，完成测试

--- logAgent/beegoConfigDemo/main.go
+++ logAgent/beegoConfigDemo/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
-	"github.com/astaxie/beego/logs"
+	"fmt"
+	"github.com/astaxie/beego/config"
 )
 
 func main() {
-	log := logs.NewLogger(10000)
-	log.SetLogger("console", "")
-	log.Trace("trace")
-	log.Info("info")
-	log.Warn("warning")
-	log.Debug("debug")
-	log.Critical("critical")
+	conf, err := config.NewConfig("ini", "D:\\tools\\logs\\logcollect.conf")
+	if err != nil {
+		fmt.Println("new config err, err:", err)
+		return
+	}
+	port, err := conf.Int("server::port")
+	if err != nil {
+		fmt.Println("config read server::port err, err:", err)
+		return
+	}
+	fmt.Println("port:", port)
+	log_level, err := conf.Int("log::log_level")
+	if err != nil {
+		fmt.Println("config read log::log_level err, err:", err)
+		return
+	}
+	fmt.Println("log_level:", log_level)
+	log_path := conf.String("log::log_path")
+	fmt.Println("log_path:", log_path)
 }
--- logAgent/tailfDemo/main.go
+++ logAgent/tailfDemo/main.go
@@ -7,11 +7,11 @@ import (
 )
 
 func main() {
-	filename := "‪D:\\tools\\logs\\mylog.log"
+	filename := "D:\\tools\\logs\\mylog.log"
 	tails, err := tail.TailFile(filename, tail.Config{
 		ReOpen:    true,
 		Follow:    true,
-		Location:  &tail.SeekInfo{Offset: 0, Whence: 2},
+		// Location:  &tail.SeekInfo{Offset: 0, Whence: 2},
 		MustExist: false,
 		Poll:      true,
 	})
@@ -19,14 +19,13 @@ func main() {
 		fmt.Println("tail file err:", err)
 		return
 	}
-	
 	var msg *tail.Line
 	var ok bool
 	for true {
 		msg, ok = <-tails.Lines
 		if !ok {
 			fmt.Printf("tail file close reopen, filename:%s\n", tails.Filename)
-			time.Sleep(1000 * time.Millisecond)
+			time.Sleep(100 * time.Millisecond)
 			continue
 		}
 		fmt.Println("msg:", msg)
