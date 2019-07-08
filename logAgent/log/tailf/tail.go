90948d8724b2f82914baf88488db0cae7ceb19bd
mutoulazy <mutoulazy@gmail.com>
2019-07-08 17:39:38 +0800

    补全tail.go 代码

--- logAgent/log/tailf/tail.go
+++ logAgent/log/tailf/tail.go
@@ -1,28 +1,56 @@
 package tailf
 
 import (
+	"errors"
 	"github.com/astaxie/beego/logs"
 	"github.com/hpcloud/tail"
 )
 
 type CollectConf struct {
 	LogPath string
-	Topic string
+	Topic   string
 }
 
+type TailfObj struct {
+	tail        *tail.Tail
+	collectConf CollectConf
+}
+
+type TiilfObjMgr struct {
+	tailfs []*TailfObj
+}
+
+var (
+	tiilfObjMgr *TiilfObjMgr
+)
+
 func InitTailf(collectConfs []CollectConf) (err error) {
-	filename := collectConfs[0].LogPath
-	tails, err := tail.TailFile(filename, tail.Config{
-		ReOpen:    true,
-		Follow:    true,
-		// Location:  &tail.SeekInfo{Offset: 0, Whence: 2},
-		MustExist: false,
-		Poll:      true,
-	})
-	if err != nil {
-		logs.Warn("tail file err:", err)
+	if len(collectConfs) == 0 {
+		err = errors.New("collectConfs len err")
 		return
 	}
-	logs.Debug(tails)
+
+	for _, v := range collectConfs {
+		tails, loadErr := tail.TailFile(v.LogPath, tail.Config{
+			ReOpen:    true,
+			Follow:    true,
+			MustExist: false,
+			Poll:      true,
+		})
+		if loadErr != nil {
+			logs.Warn("tail file err:", loadErr)
+			err = loadErr
+			return
+		}
+
+		tailfObj := &TailfObj{
+			collectConf: v,
+			tail:        tails,
+		}
+		// tailfObj.tail = tails
+
+		tiilfObjMgr.tailfs = append(tiilfObjMgr.tailfs, tailfObj)
+	}
+
 	return
 }
